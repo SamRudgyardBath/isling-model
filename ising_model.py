3fb1a8f9922643de1624292cab1bf759db577e3b
SamRudgyardBath <sr2116@bath.ac.uk>
2023-08-18 17:44:58 +0100

    Implement GetEnergy function

--- ising_model.py
+++ ising_model.py
@@ -10,6 +10,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 gridLength = 100
+
+def GetEnergy(index):
+    thisSpin = lattice[index]
+    topSpin = lattice[index - gridLength]
+    bottomSpin = lattice[index + gridLength]
+    leftSpin = lattice[index - 1]
+    rightSpin = lattice[index + 1]
+    
+    neighbours = [topSpin, bottomSpin, leftSpin, rightSpin]
+    energy = 0
+    
+    for neighbouringSpin in neighbours:
+        energy -= thisSpin * neighbouringSpin
+    
+    return energy
+
 # Initialise lattice structure
 lattice = np.zeros((gridLength, gridLength)) 
 randomArray = np.random.random((gridLength, gridLength))
