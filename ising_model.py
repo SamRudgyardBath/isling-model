cd34ef16c0fd54b308e6e4710d4e77f4fef8548e
SamRudgyardBath <sr2116@bath.ac.uk>
2023-08-19 18:43:19 +0100

    Implement GetTotalEnergy() for the system

--- ising_model.py
+++ ising_model.py
@@ -11,12 +11,13 @@ import matplotlib.pyplot as plt
 
 gridLength = 100
 
-def GetEnergy(index):
-    thisSpin = lattice[index]
-    topSpin = lattice[index - gridLength]
-    bottomSpin = lattice[index + gridLength]
-    leftSpin = lattice[index - 1]
-    rightSpin = lattice[index + 1]
+def GetEnergy(x, y):
+    thisSpin = lattice[x, y]
+    
+    topSpin = lattice[x, y - 1] if y != 0 else 0
+    bottomSpin = lattice[x, y + 1] if y != gridLength-1 else 0
+    leftSpin = lattice[x - 1, y] if x != 0 else 0
+    rightSpin = lattice[x + 1, y] if x != gridLength-1 else 0
     
     neighbours = [topSpin, bottomSpin, leftSpin, rightSpin]
     energy = 0
@@ -26,10 +27,18 @@ def GetEnergy(index):
     
     return energy
 
+def GetTotalEnergy():
+    totEnergy = 0
+    for x in range(0, gridLength):
+        for y in range(0, gridLength):
+            totEnergy += GetEnergy(x, y)
+    return totEnergy
+
 # Initialise lattice structure
 lattice = np.zeros((gridLength, gridLength)) 
 randomArray = np.random.random((gridLength, gridLength))
 lattice[randomArray >= 0.75] = 1
 lattice[randomArray < 0.75] = -1
 
+print(f'Total Energy: {GetTotalEnergy()} J')
 plt.imshow(lattice)
