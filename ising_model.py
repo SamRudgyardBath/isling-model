6a7f6fbfb21c8d0b4f77a6f0769b7b6c9fbd6d9e
SamRudgyardBath <sr2116@bath.ac.uk>
2023-08-24 15:37:48 +0100

    Introduce class for Lattice

--- ising_model.py
+++ ising_model.py
@@ -9,85 +9,110 @@ Created on Fri Aug 18 17:22:03 2023
 import numpy as np
 import matplotlib.pyplot as plt
 
-gridLength = 100
-maxTime = 100000
-beta = 1
+gridLength = 50
+maxTime = 1000000
+beta = 0.2
 
-def GetEnergy(x, y):
-    thisSpin = lattice[x, y]
-    
-    topSpin = lattice[x, y-1] if y != 0 else 0
-    bottomSpin = lattice[x, y+1] if y != gridLength-1 else 0
-    leftSpin = lattice[x-1, y] if x != 0 else 0
-    rightSpin = lattice[x+1, y] if x != gridLength-1 else 0
-    
-    neighbours = [topSpin, bottomSpin, leftSpin, rightSpin]
-    energy = 0
-    
-    for neighbouringSpin in neighbours:
-        energy -= thisSpin * neighbouringSpin
-    
-    return energy
 latticePlot = np.zeros((4, gridLength, gridLength))
 
-def GetTotalEnergy():
-    totEnergy = 0
-    for x in range(0, gridLength):
-        for y in range(0, gridLength):
-            totEnergy += GetEnergy(x, y)
-    return totEnergy
-
-def Metropolis():
-    for t in range(0, maxTime):
-        # Pick a random particle to flip
-        x = np.random.randint(0, gridLength)
-        y = np.random.randint(0, gridLength)
-        initialSpin = lattice[x, y]
-        proposedSpin = initialSpin * -1
+class Lattice():
+    def __init__(self, gridLength, maxTime, beta):
+        self.gridLength = gridLength
+        self.maxTime = maxTime
+        self.beta = beta
         
-        # Calculate Initial Energy
-        initialEnergy = 0
-        initialEnergy += -initialSpin * lattice[x, y-1] if y != 0 else 0
-        initialEnergy += -initialSpin * lattice[x, y+1] if y != gridLength-1 else 0
-        initialEnergy += -initialSpin * lattice[x-1, y] if x != 0 else 0
-        initialEnergy += -initialSpin * lattice[x+1, y] if x != gridLength-1 else 0
+        self.lattice = np.zeros((gridLength, gridLength))
+        
+    def GetEnergy(self, x, y):
+        thisSpin = self.lattice[x, y]
+        
+        topSpin = self.lattice[x, y-1] if y != 0 else 0
+        bottomSpin = self.lattice[x, y+1] if y != gridLength-1 else 0
+        leftSpin = self.lattice[x-1, y] if x != 0 else 0
+        rightSpin = self.lattice[x+1, y] if x != gridLength-1 else 0
+        
         neighbours = [topSpin, bottomSpin, leftSpin, rightSpin]
         energy = 0
         
-        # Calculate Final Energy
-        finalEnergy = 0
-        finalEnergy += -proposedSpin * lattice[x, y-1] if y != 0 else 0
-        finalEnergy += -proposedSpin * lattice[x, y+1] if y != gridLength-1 else 0
-        finalEnergy += -proposedSpin * lattice[x-1, y] if x != 0 else 0
-        finalEnergy += -proposedSpin * lattice[x+1, y] if x != gridLength-1 else 0
         for neighbouringSpin in neighbours:
             energy -= thisSpin * neighbouringSpin
         
-        dE = finalEnergy - initialEnergy
-        if (dE > 0) * (np.exp(-beta * (finalEnergy - initialEnergy)) > np.random.random()):
-            lattice[x,y] = proposedSpin # Accept the proposed state!
         return energy
+    
+    def GetTotalEnergy(self):
+        totEnergy = 0
+        for x in range(0, gridLength):
+            for y in range(0, gridLength):
+                totEnergy += self.GetEnergy(x, y)
+        return totEnergy
+    
+    def CalcMag(self):
+        return np.sum(self.lattice)
+
+    def Metropolis(self, lattice):
+        totEnergyVals = np.zeros(maxTime-1)
+        totSpinVals = np.zeros(maxTime-1)
+        for t in range(0, maxTime-1):
+            # Pick a random particle to flip
+            x = np.random.randint(0, gridLength)
+            y = np.random.randint(0, gridLength)
+            initialSpin = self.lattice[x, y]
+            proposedSpin = initialSpin * -1
             
-        elif dE <= 0:
-            lattice[x,y] = proposedSpin # Accept the proposed state!
+            energy = self.GetEnergy(x, y)
             
-def CalcMag():
-    return np.sum(lattice)
+            # Calculate Initial Energy
+            initialEnergy = 0
+            initialEnergy += -initialSpin * self.lattice[x, y-1] if y != 0 else 0
+            initialEnergy += -initialSpin * self.lattice[x, y+1] if y != gridLength-1 else 0
+            initialEnergy += -initialSpin * self.lattice[x-1, y] if x != 0 else 0
+            initialEnergy += -initialSpin * self.lattice[x+1, y] if x != gridLength-1 else 0
+            
+            # Calculate Final Energy
+            finalEnergy = 0
+            finalEnergy += -proposedSpin * self.lattice[x, y-1] if y != 0 else 0
+            finalEnergy += -proposedSpin * self.lattice[x, y+1] if y != gridLength-1 else 0
+            finalEnergy += -proposedSpin * self.lattice[x-1, y] if x != 0 else 0
+            finalEnergy += -proposedSpin * self.lattice[x+1, y] if x != gridLength-1 else 0
+            
+            dE = finalEnergy - initialEnergy
+            if (dE > 0) * (np.exp(-beta * (finalEnergy - initialEnergy)) > np.random.random()):
+                self.lattice[x,y] = proposedSpin # Accept the proposed state!
+                energy += dE
+                
+            elif dE <= 0:
+                self.lattice[x,y] = proposedSpin # Accept the proposed state!
+                energy += dE
+                
+            totEnergyVals[t] = energy
+            totSpinVals[t] = self.CalcMag()
+            
+            if t == 0:
+                latticePlot[0] = self.lattice
+            elif t == int(maxTime/4):
+                latticePlot[1] = self.lattice
+            elif t == int(maxTime/2):
+                latticePlot[2] = self.lattice
+            elif t == int(3 * maxTime / 4):
+                latticePlot[3] = self.lattice
+                
+        return totEnergyVals, totSpinVals
             
 
 # Initialise lattice structure
-lattice = np.zeros((gridLength, gridLength)) 
+lattice = Lattice(50, 10000, 0.2)
 randomArray = np.random.random((gridLength, gridLength))
-lattice[randomArray >= 0.75] = 1
-lattice[randomArray < 0.75] = -1
+lattice.lattice[randomArray >= 0.5] = 1
+lattice.lattice[randomArray < 0.5] = -1
 
-print(f'Total Energy at time t=0: {GetTotalEnergy()} J')
-print(f'Total Magnetisation at time t=0: {CalcMag()}')
+print(f'Total Energy at time t=0: {lattice.GetTotalEnergy()} J')
+print(f'Total Magnetisation at time t=0: {lattice.CalcMag()}')
 
-Metropolis()
+totEnergyVals, totSpinVals = lattice.Metropolis(lattice)
 
-print(f'Total Energy at time t={maxTime}: {GetTotalEnergy()} J')
-print(f'Total Magnetisation at time t={maxTime}: {CalcMag()}')
+print(f'Total Energy at time t={maxTime}: {lattice.GetTotalEnergy()} J')
+print(f'Total Magnetisation at time t={maxTime}: {lattice.CalcMag()}')
+
 fig, axes = plt.subplots(1, 2, figsize=(12,4))
 ax = axes[0]
 ax.plot(totSpinVals/gridLength**2)
